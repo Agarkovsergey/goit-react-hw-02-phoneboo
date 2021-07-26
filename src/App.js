905cf3918a7186600a24e80bb640c10c313bc025
Agarkovsergey <sergeyagarkov91@mail.ru>
2021-07-26 21:05:16 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
+import ContactForm from './components/contactForm/ContactForm'
+import ContactList from './components/contactList/ContactList'
+import Filter from './components/filter/Filter'
+
+
+
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: ''
+  }
+
+  createContact = ({ name, number }) => {
+    const contact = {
+      id: uuidv4(),
+      name,
+      number
+    }
+
+
+    const foundContact = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    
+    if (foundContact) {
+      alert(`${name} is already in contacts`);
+      return
+    }
+    
+    this.setState({
+      contacts: [
+        ...this.state.contacts,
+        contact
+      ]
+    })
+  }
+
+  onFilterChange = ({ target }) => {
+    this.setState({
+      filter: target.value
+    });
+  }
+
+  deleteContact = ({ target }) => {
+    const id = target.id.split(':')[1];
+    console.log(id);
+    this.setState({
+      contacts: this.state.contacts.filter(
+        contact => contact.id !== id
+      ),
+    })
+  }
+
+  render() {
+    const filteredContacts = this.state.contacts.filter(
+      contact => contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+
+    return (
+      <div className="wrapper">
+        <h2 className="lessonHeader">Phonebook</h2>
+        <ContactForm
+          createContact={this.createContact}
+        />
+        <h2 className="lessonHeader">Contacts</h2>
+        <Filter handleChange={this.onFilterChange} filter={this.state.filter}/>
+        <ContactList contacts={filteredContacts} handleDelete={ this.deleteContact }/>
+      </div>
+
+    );
+  }
+}
+
+export default App;
